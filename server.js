1ee850cbe29b2ba74dae7a2f1469815c22d16659
Starzz <ofbloom6@gmail.com>
2025-07-12 20:16:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+
 const app = express();
 
-// Serve static files (CSS/JS/images)
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
+app.use(express.json());
 
 // API Routes
 app.use('/api/keys', require('./api/keys'));
 
-// Serve Horizon login page for ALL routes
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle SPA routing
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
